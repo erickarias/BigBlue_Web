948f1bab089d468b8f35a96b0fd0d02c830b8e12
Erick Arias <searchlightdesigns@gmail.com>
2016-05-20 00:23:53 -0500

    JS and CSS For sticky header and sticky anchor links Moblie nav styling, bubbles background, transition effects for sticky navs, anchor links,

--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -8,6 +8,7 @@
 <script src="https://use.typekit.net/qwb6xkj.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
   <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
   <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
   <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css"/>
@@ -28,7 +29,7 @@
 <script type-"text/javascript">
   $(document).ready(function(){
     $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
-      $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+      $('.sliding-panel-content,.sliding-panel-fade-screen, .sliding-panel-button').toggleClass('is-visible');
       e.preventDefault();
     });
   });
@@ -39,7 +40,7 @@
       $(window).scroll(function() {
           var scroll = $(window).scrollTop();
 
-          if (scroll >= 50) {
+          if (scroll >= 2) {
               $('#header').removeClass('top-header').addClass("scroll-header");
           } else {
               $('#header').removeClass("scroll-header").addClass('top-header');
@@ -48,28 +49,37 @@
   });
 </script>
 <script type-"text/javascript">
-  $(function() {
-      //caches a jQuery object containing the header element
-      $(window).scroll(function() {
-          var scroll = $(window).scrollTop();
-
-          if (scroll >= 50) {
-              $('#header').removeClass('top-header').addClass("scroll-header");
-          } else {
-              $('#header').removeClass("scroll-header").addClass('top-header');
-          }
-      });
+$(document).ready(function(){
+  $("a.square-1").click(function() {
+      var targetDiv = $(".square-row-1");
+      $('html, body').animate({
+          scrollTop: $(targetDiv).offset().top - 100
+      }, 1000, 'easeOutExpo');
   });
+  $("a.square-2").click(function() {
+      var targetDiv = $(".square-row-2");
+      $('html, body').animate({
+          scrollTop: $(targetDiv).offset().top - 100
+      }, 1000, 'easeOutExpo');
+  });
+  $("a.square-3").click(function() {
+      var targetDiv = $(".square-row-3");
+      $('html, body').animate({
+          scrollTop: $(targetDiv).offset().top - 100
+      }, 1000, 'easeOutExpo');
+  });
+});
 </script>
 <script type-"text/javascript">
   $(function() {
     var stickyTop = $('#home-anchors').offset().top - 54;
+    var stickyBottom = $('#mobile-app').offset().top;
     $(window).on( 'scroll', function(){
-      if ($(window).scrollTop() >= stickyTop) {
-        $('#home-anchors').css({position: "fixed", top: "85px", width: "100%"});
+      if ($(window).scrollTop() >= stickyTop && $(window).scrollTop() <= stickyBottom) {
+        $('#home-anchors').css({position: "fixed", top: "54px", width: "100%"});
         $('#home-squares').css({margin: "54px 0 0 0"});
-        
-      } else {
+      }
+      else {
         $('#home-anchors').css({position: "relative", top: "0px"});
         $('#home-squares').css({margin: "0"});
       }
@@ -81,7 +91,7 @@
 <div id="wrapper" class="hfeed">
   <header id="header" role="banner" class="top-header">
     <div id="desktop-nav">
-      <div id="signup">Sign up now!</div>
+      <a href=""class="signup">Sign up now!</a>
       <section id="branding">
         <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
           <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home">
@@ -101,13 +111,16 @@
     </div>
     <div id="mobile-nav">
       <img src="wp-content/themes/blankslate/images/svg/BBSS_logo_rgb.svg" class="logo"/>
-      <i class="fa fa-bars js-menu-trigger sliding-panel-button"></i>
-
+      <div class="js-menu-trigger sliding-panel-button">
+        <i class="fa fa-bars"></i>
+        <i class="fa fa-close"></i>
+      </div>
       <nav class="js-menu sliding-panel-content">
+        <?php wp_nav_menu( array('theme_location' => 'main-menu')); ?>
         <ul>
-          <li><a href="javascript:void(0)">Item 1</a></li>
-          <li><a href="javascript:void(0)">Item 2</a></li>
-          <li><a href="javascript:void(0)">Item 3</a></li>
+          <li class="secondary-menu-item"><a href="">Contact Us</a></li>
+          <li class="secondary-menu-item"><a href="">Parent Portal</a></li>
+          <li class="signup"><a href="">Sign Up Now!</a></li>
         </ul>
       </nav>
 
